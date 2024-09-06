a08e1178a8f233697217dfd6096ea9ae51575b3e
nhathaoGremsy <hao.nguyen@gremsy.com>
2024-09-06 13:21:16 +0700

    first commit

---
+++ sony_sdk/build/CMakeFiles/RemoteCli.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/CameraDevice.cpp" "CMakeFiles/RemoteCli.dir/app/CameraDevice.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/CameraDevice.cpp.o.d"
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/ConnectionInfo.cpp" "CMakeFiles/RemoteCli.dir/app/ConnectionInfo.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/ConnectionInfo.cpp.o.d"
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/MessageDefine.cpp" "CMakeFiles/RemoteCli.dir/app/MessageDefine.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/MessageDefine.cpp.o.d"
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/PropertyValueTable.cpp" "CMakeFiles/RemoteCli.dir/app/PropertyValueTable.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/PropertyValueTable.cpp.o.d"
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/RemoteCli.cpp" "CMakeFiles/RemoteCli.dir/app/RemoteCli.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/RemoteCli.cpp.o.d"
+  "/home/software-03/hao_nguyen/btvl/part_1.1/sony_sdk/CrSDK_v1.12.00_20240425a_Linux64PC/app/Text.cpp" "CMakeFiles/RemoteCli.dir/app/Text.cpp.o" "gcc" "CMakeFiles/RemoteCli.dir/app/Text.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
